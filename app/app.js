bc01210c75871af4baba018eccf46b31349599e3
artdrozdov <artem.drozdoff@gmail.com>
2017-01-12 00:18:59 +0200

    sections

--- app/app.js
+++ app/app.js
@@ -1,5 +1,29 @@
 angular.module("components", []).directive("navigation", function() {
-    return { restrict: 'E', templateUrl: '/app/components/navigation.html' }
+    return { restrict: 'E', templateUrl: '/app/components/navigation.html', replace: true }
 });
 
-angular.module("Landing", ["components"]);
+angular.module("components").directive("slide1", function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/app/components/slide1.html',
+        replace: true,
+    }
+});
+
+angular.module("components").directive("slide2", function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/app/components/slide2.html',
+        replace: true,
+    }
+});
+
+angular.module("components").directive("slide3", function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/app/components/slide3.html',
+        replace: true,
+    }
+});
+
+var app = angular.module("Landing", ["components"]);
